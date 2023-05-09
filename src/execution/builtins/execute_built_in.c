b8a6f5401c447da4c0716eb26d215f71fabaa374
Bill42qc <bill.42qc@gmail.com>
2023-05-09 15:53:19 -0400

    Merge branch 'master' of github.com:Bill42qc/minishell

---
+++ src/execution/builtins/execute_built_in.c
@@ -0,0 +1,23 @@
+#include "minishell.h"
+
+void	execute_built_in(t_cmd *cmd , t_data *data)
+{
+	size_t	len;
+
+	printf("you have use a built in command \n");
+	len = ft_strlen(cmd->name);
+	if (ft_strnstr(cmd->name, "echo", len) != 0)
+		echo_cmd (cmd);
+	if (ft_strnstr(cmd->name, "env", len) != 0)
+		env_cmd(data);
+	if (ft_strnstr(cmd->name, "pwd", len) != 0)
+		pwd_cmd(cmd);
+	if (ft_strnstr(cmd->name, "cd", len) != 0)
+		cd_cmd(cmd);
+	if (ft_strnstr(cmd->name, "export", len) != 0)
+		export(cmd);
+	if (ft_strnstr(cmd->name, "unset", len) != 0)
+		unset_cmd(cmd);
+	if (ft_strnstr(cmd->name, "exit", len) != 0)
+		exit_cmd (0);
+}
