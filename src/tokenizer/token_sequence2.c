f468db4c3a695c193275b9e71a2373f5ed568501
Bill42qc <bill.42qc@gmail.com>
2023-07-25 14:58:18 -0400

    short token_sequence.c

--- src/tokenizer/token_sequence2.c
+++ src/tokenizer/token_sequence2.c
@@ -12,13 +12,29 @@
 
 #include "minishell.h"
 
+int32_t	process_sequence(int32_t i, t_token *parent, t_token_type tk_type,
+		char *tk)
+{
+	t_token_type	type;
+	int32_t			t_len;
+
+	type = get_token_type(&parent->str[i]);
+	t_len = get_token_len(&parent->str[i], type);
+	if (tk_type != type && is_token_delimiter(type))
+		i = skip_token_delimiter(type, i, parent);
+	else if (type == tk_type)
+		i = add_sequence_token(i, tk, type, parent);
+	else
+		i += t_len;
+	return (i);
+}
+
 t_token	*sequence_tokenizer(char *tk, t_token *parent)
 {
 	int32_t			i;
-	t_token_type	type;
-	int32_t			t_len;
 	t_token_type	tk_type;
 
+	i = 0;
 	i = 0;
 	tk_type = get_token_type(tk);
 	if (!has_token_sequence(parent))
@@ -28,19 +44,44 @@ t_token	*sequence_tokenizer(char *tk, t_token *parent)
 	{
 		if (has_error())
 			return (parent->child);
-		type = get_token_type(&parent->str[i]);
-		t_len = get_token_len(&parent->str[i], type);
-		if (tk_type != type && is_token_delimiter(type))
-			i = skip_token_delimiter(type, i, parent);
-		else if (type == tk_type)
-			i = add_sequence_token(i, tk, type, parent);
-		else
-			i += t_len;
+		i = process_sequence(i, parent, tk_type, tk);
+		if (i == -1)
+			return (parent->child);
 	}
 	add_tk("", TK_END, i, parent);
 	split_token_sequence(parent);
 	return (parent->child);
 }
+
+// t_token	*sequence_tokenizer(char *tk, t_token *parent)
+// {
+// 	int32_t			i;
+// 	t_token_type	type;
+// 	int32_t			t_len;
+// 	t_token_type	tk_type;
+
+// 	i = 0;
+// 	tk_type = get_token_type(tk);
+// 	if (!has_token_sequence(parent))
+// 		return (NULL);
+// 	add_sequence_token(i, "", tk_type, parent);
+// 	while (parent->str[i])
+// 	{
+// 		if (has_error())
+// 			return (parent->child);
+// 		type = get_token_type(&parent->str[i]);
+// 		t_len = get_token_len(&parent->str[i], type);
+// 		if (tk_type != type && is_token_delimiter(type))
+// 			i = skip_token_delimiter(type, i, parent);
+// 		else if (type == tk_type)
+// 			i = add_sequence_token(i, tk, type, parent);
+// 		else
+// 			i += t_len;
+// 	}
+// 	add_tk("", TK_END, i, parent);
+// 	split_token_sequence(parent);
+// 	return (parent->child);
+// }
 
 t_token	*sequences_tokenizer(t_token *parent)
 {
