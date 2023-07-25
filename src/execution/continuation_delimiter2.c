4b7bddace1b4b5d8d3841cc6b503a850c9f3a25d
Bill42qc <bill.42qc@gmail.com>
2023-07-25 16:07:47 -0400

    short continuation_dleemiter2.c

--- src/execution/continuation_delimiter2.c
+++ src/execution/continuation_delimiter2.c
@@ -41,39 +41,6 @@ void	exec_delimiter_continuation(char *delimiter, t_token *parent)
 	free_continuation();
 }
 
-void	exec_continuation(t_token *parent)
-{
-	t_process	*proc;
-	char		buffer[128];
-	char		*str;
-	size_t		size;
-
-	buffer[0] = '\0';
-	proc = get_process();
-	proc->continuation = open_write_continuation();
-	write_non_empty_continuation();
-	if (proc->errnum > 0)
-		return (free_continuation());
-	open_read_temp_file(proc->continuation);
-	str = malloc(2);
-	if (!str)
-		return (free_all_and_exit2(errno, "malloc error"));
-	str[0] = ' ';
-	str[1] = '\0';
-	size = 1;
-	while (size > 0)
-	{
-		size = read(proc->continuation->fd, buffer, 127);
-		if (size == (size_t) - 1)
-			return (free(str), free_all_and_exit2(errno, "read error"));
-		buffer[size] = '\0';
-		str = ft_strjoinfree(str, &buffer[0]);
-	}
-	parent->str = ft_strjoinfree(parent->str, str);
-	free_ptr(str);
-	free_continuation();
-}
-
 int32_t	write_delimiter_continuation(const char *delimiter, t_redirect *redir)
 {
 	pid_t		pid;
@@ -96,3 +63,90 @@ int32_t	write_delimiter_continuation(const char *delimiter, t_redirect *redir)
 	proc->execution = EXEC_END;
 	return (proc->errnum);
 }
+
+void	open_continuation(t_process *proc)
+{
+	proc->continuation = open_write_continuation();
+	write_non_empty_continuation();
+	if (proc->errnum > 0)
+	{
+		free_continuation();
+		return ;
+	}
+	open_read_temp_file(proc->continuation);
+}
+
+char	*read_continuation(int32_t fd)
+{
+	char	buffer[128];
+	char	*str;
+	size_t	size;
+
+	str = malloc(2);
+	if (!str)
+		return (free_all_and_exit2(errno, "malloc error"), NULL);
+	str[0] = ' ';
+	str[1] = '\0';
+	size = 1;
+	while (size > 0)
+	{
+		size = read(fd, buffer, 127);
+		if (size == (size_t) - 1)
+		{
+			free(str);
+			free_all_and_exit2(errno, "read error");
+			return (NULL);
+		}
+		buffer[size] = '\0';
+		str = ft_strjoinfree(str, &buffer[0]);
+	}
+	return (str);
+}
+
+void	exec_continuation(t_token *parent)
+{
+	t_process	*proc;
+	char		*str;
+
+	proc = get_process();
+	open_continuation(proc);
+	if (!proc->continuation)
+		return ;
+	str = read_continuation(proc->continuation->fd);
+	parent->str = ft_strjoinfree(parent->str, str);
+	free_ptr(str);
+	free_continuation();
+}
+
+// void	exec_continuation(t_token *parent)
+// {
+// 	t_process	*proc;
+// 	char		buffer[128];
+// 	char		*str;
+// 	size_t		size;
+
+// 	buffer[0] = '\0';
+// 	proc = get_process();
+// 	proc->continuation = open_write_continuation();
+// 	write_non_empty_continuation();
+// 	if (proc->errnum > 0)
+// 		return (free_continuation());
+// 	open_read_temp_file(proc->continuation);
+// 	str = malloc(2);
+// 	if (!str)
+// 		return (free_all_and_exit2(errno, "malloc error"));
+// 	str[0] = ' ';
+// 	str[1] = '\0';
+// 	size = 1;
+// 	while (size > 0)
+// 	{
+// 		size = read(proc->continuation->fd, buffer, 127);
+// 		if (size == (size_t) - 1)
+// 			return (free(str), free_all_and_exit2(errno, "read error"));
+// 		buffer[size] = '\0';
+// 		str = ft_strjoinfree(str, &buffer[0]);
+// 	}
+// 	parent->str = ft_strjoinfree(parent->str, str);
+// 	free_ptr(str);
+// 	free_continuation();
+// }
